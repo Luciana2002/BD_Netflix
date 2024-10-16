a5ae6cddbcf55bbb4162be608a3926ef09f0e77e
Luciana Itati Fernandez Lezcano <118312382+Luciana2002@users.noreply.github.com>
2024-10-16 13:36:06 -0300

    Update SQLQuery1_netflix_insert.sql

--- SQLQuery1_netflix_insert.sql
+++ SQLQuery1_netflix_insert.sql
@@ -37,7 +37,7 @@ ORDER BY descripcion ASC
 select * from pais
 
 ---------------------
--- Tabla Categoría --
+-- Tabla CategorÃ­a --
 ---------------------
 
 INSERT INTO categoria (descripcion)
@@ -79,7 +79,7 @@ select * from director
 -- Tabla Show --
 ----------------
 
-INSERT INTO Show (id_show, titulo, fecha_salida, duracion, año_lanzamiento, descripcion, id_tipo, id_rating)
+INSERT INTO Show (id_show, titulo, fecha_salida, duracion, aÃ±o_lanzamiento, descripcion, id_tipo, id_rating)
 SELECT show_id, 
 	   title, 
        date_added, 
@@ -151,54 +151,3 @@ CROSS APPLY STRING_SPLIT(n.cast, ',') AS s
 JOIN actor a ON TRIM(s.value) = a.nombre_apellido
 
 select * from elenco
-
------------------------------------------------------------
----------------- Consultas de virificacion ---------------- 
------------------------------------------------------------
-
--- 1) Cantidad de directores con mas de 10 películas o series dirigidas
-
-SELECT d.nombre_apellido, COUNT(sd.id_show) AS 'Cantidad Películas o Series'
-FROM show_director sd
-INNER JOIN director d ON d.id_director = sd.id_director
-GROUP BY d.nombre_apellido
-HAVING COUNT(sd.id_show) > 10
-ORDER BY COUNT(sd.id_show) DESC
-
--- Verificacion utilizando la tabla netflix_titles --
-SELECT TRIM(d.value) AS director, COUNT(nt.title) AS total_titulos
-FROM netflix_titles nt
-CROSS APPLY STRING_SPLIT(nt.director, ',') AS d
-WHERE nt.director IS NOT NULL   
-GROUP BY TRIM(d.value)           
-HAVING COUNT(nt.title) > 10 
-ORDER BY total_titulos DESC       
-
--- 2) El actor con mayor participación en películas o series
-
-SELECT TOP 1 a.nombre_apellido, COUNT(e.id_show) AS 'Cantidad Películas o Series'
-FROM elenco e
-INNER JOIN actor a ON a.id_actor = e.id_actor
-GROUP BY a.nombre_apellido
-HAVING COUNT(e.id_show) > 10
-ORDER BY COUNT(e.id_show) DESC
-
--- Verificacion utilizando la tabla netflix_titles --
-SELECT TOP 1 TRIM(a.value) AS actor, COUNT(nt.title) AS total_titulos
-FROM netflix_titles nt
-CROSS APPLY STRING_SPLIT(nt.cast, ',') AS a
-WHERE nt.cast IS NOT NULL   
-GROUP BY TRIM(a.value)           
-HAVING COUNT(nt.title) > 10 
-ORDER BY total_titulos DESC  
-
--- 3) Cantidad de series añadidas en los últimos cinco años
-
-SELECT COUNT(*) AS 'Cantidad de Series en los últimos cinco años'
-FROM Show 
-WHERE año_lanzamiento >= YEAR(GETDATE()) - 5
-
--- Verificacion utilizando la tabla netflix_titles --
-SELECT COUNT(*) AS cantidad_series_peliculas
-FROM netflix_titles
-WHERE  release_year >= YEAR(GETDATE()) - 5
